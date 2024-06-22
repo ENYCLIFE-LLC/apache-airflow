4e83a2710f921c294115d46b30e87aeee75633f5
Jinyu Chen <jinyu2010.chen@gmail.com>
2024-06-22 15:44:55 -0500

    Pipfile

--- constraints.sh
+++ constraints.sh
@@ -1,6 +1,5 @@
-AIRFLOW_VERSION=2.7.1
+AIRFLOW_VERSION=2.9.2
 PYTHON_VERSION=3.9
-PIPENV_VENV_IN_PROJECT=1
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 
 
